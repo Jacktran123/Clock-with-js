679e551b6b0719477da6be7ed994bb190f4e2658
Khoa Tran <jacktran1799@gmaail.com>
2020-04-20 13:57:07 -0500

    finished version

---
+++ index.js
@@ -0,0 +1,27 @@
+const hour=document.querySelector('.hour');
+const minute=document.querySelector('.min');
+const second=document.querySelector('.sec');
+
+
+
+
+function setDate(){
+    const now= new Date();
+   
+    const seconds = now.getSeconds();
+    const degree= ((seconds / 60) * 360);
+    console.log(seconds);
+    second.style.transform=`rotate(${degree-90}deg)`;
+
+    const minutes= now.getMinutes();
+    const degreeMinutes= ((minutes / 60) * 360) + ((seconds/60)*6);
+    console.log(minutes);
+    minute.style.transform=`rotate(${degreeMinutes-90}deg)`;
+
+    const hours=now.getHours();
+    const degreeHour= ((hours / 12) * 360) + ((minutes/60)*30) ;
+    hour.style.transform=`rotate(${degreeHour-90}deg)`;
+}
+
+
+setInterval(setDate,1000);
